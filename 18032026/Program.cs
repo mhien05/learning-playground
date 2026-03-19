b48f403be65cf7f3f7467d2233acc178d567e2db
HienTran <hientm0978@gmail.com>
2026-03-19 11:46:37 +0700

    18/03/2026 - ADO.NET basic

---
+++ 18032026/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace _18032026
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TestDbConnection();
+        }
+
+        public static void TestDbConnection()
+        {
+            var conn = new SqlConnection("Server=.;Database=HR;Trusted_Connection=yes; TrustServerCertificate=True");
+            conn.Open();
+
+            Console.WriteLine("Connect to db successfull");
+
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "select employee_id, email from employees";
+            
+            using var reader = cmd.ExecuteReader();
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"ID: {reader.GetInt32(0)}, Name: {reader.GetString(1)}");
+                }
+            }
+
+            conn.Close(); 
+        }
+    }
+}
