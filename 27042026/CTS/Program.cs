bb24ca4f0de60b36b1fb2cbb7e78e2f4e898d93d
hindeptrai <hientm0978@gmail.com>
2026-04-27 14:14:58 +0700

    CTS in .NET

---
+++ 27042026/CTS/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CTS
+{
+    public class Person
+    {
+        public string Name;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // #VALUE TYPE
+            int x = 10;
+            int y = x;
+            y = 20;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            Person p1 = new Person();
+            p1.Name = "an";
+
+            Person p2 = p1;
+            p2.Name = "binh";
+
+            Console.WriteLine(p1.Name); // print out "binh"
+        }
+    }
+}
